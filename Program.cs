be1e3ca886c51a6e7cc9acc178e961bf17013d60
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-05-08 09:03:33 -0700

    first comit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HighestRankedCard
+{
+    enum Cards
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Clubs
+    {
+        Cards[] clubCards = { };
+    }
+}
